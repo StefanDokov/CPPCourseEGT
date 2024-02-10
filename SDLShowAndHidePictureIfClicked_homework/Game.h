da6e8cc71c87a6fdabf3c8f11ec4fa0aa5dd6b47
StefanDokov <stefandokov2211@gmail.com>
2024-02-10 11:46:28 +0200

    Add sdl homework
    Add sdl homework

---
+++ SDLShowAndHidePictureIfClicked_homework/Game.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+#include "TextureManager.h"
+#include "Shapes.h"
+#include <vector>
+
+
+class Game {
+public:
+	Game();
+	~Game();
+
+	bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool isRunning();
+	std::vector<bool> windws;
+private:
+	SDL_Window* window = NULL;
+	SDL_Renderer* renderer = NULL;
+	bool running;
+	int currentFrame;
+	int currentRow;
+	int mouseDownX, mouseDownY;
+};
